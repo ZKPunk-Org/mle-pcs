1cfc4728b6adee316bf7ec9b1177d9661241c0bd
Kurt Pan <74859307+kurtpan666@users.noreply.github.com>
2025-06-12 14:32:11 +0800

    no notify.yml

--- .github/workflows/escape_markdown.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script escapes special Markdown characters for Telegram API
-# Usage:
-#   cat input.txt | ./escape_markdown.sh > output.txt
-#   OR
-#   echo "Your text here" | ./escape_markdown.sh
-
-perl -pe 's/([_*[\]()~`>#+\-=\|{}.!])/\\$1/g'
